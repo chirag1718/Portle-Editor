acf0f23eb271bdc5f4f1373b4fa1dc29a6c60473
chirag sonar <chiragsonar1718@gmail.com>
2024-01-11 18:21:09 +0530

    feat: added functionality to save content in local storage

--- src/app/components/Editor.tsx
+++ src/app/components/Editor.tsx
@@ -1,24 +1,38 @@
 "use client";
-import { ContentState, Editor, EditorState } from "draft-js";
-import React, { FC, useRef, useState } from "react";
+import {
+  ContentState,
+  Editor,
+  EditorState,
+  convertFromRaw,
+  convertToRaw,
+} from "draft-js";
+import React, { FC, useEffect, useRef, useState } from "react";
 import Decorator from "./Decorator";
 import "draft-js/dist/Draft.css";
+
 export interface EditorProps {
   initialValue?: string;
   onChange?: (value: string) => void;
   style?: React.CSSProperties;
-  spellCheck?: boolean;
 }
 const CustomEditor: FC<EditorProps> = ({ initialValue, onChange, style }) => {
-  const [editorState, setEditorState] = useState(() =>
-    initialValue
-      ? EditorState.createWithContent(
-          ContentState.createFromText(initialValue),
-          Decorator
-        )
-      : EditorState.createEmpty(Decorator)
-  );
-  const editorRef = useRef<EditorState | null>(null);
+  const [editorState, setEditorState] = useState(() => {
+    const storedContent = localStorage.getItem("editorContent");
+    if (storedContent) {
+      const rawContent = JSON.parse(storedContent);
+      return EditorState.createWithContent(
+        convertFromRaw(rawContent),
+        Decorator
+      );
+    } else {
+      return initialValue
+        ? EditorState.createWithContent(
+            ContentState.createFromText(initialValue),
+            Decorator
+          )
+        : EditorState.createEmpty(Decorator);
+    }
+  });
   const handleChange = (newEditorState: EditorState): void => {
     setEditorState(newEditorState);
     if (onChange) {
@@ -31,12 +45,27 @@ const CustomEditor: FC<EditorProps> = ({ initialValue, onChange, style }) => {
       );
     }
   };
+  const handleSave = () => {
+    const contentState = editorState.getCurrentContent();
+    const rawContent = convertToRaw(contentState);
+    localStorage.setItem("editorContent", JSON.stringify(rawContent));
+  };
+
   return (
-    <div style={style}>
+    <div style={style} className="bg-stone-800 p-5 mr-5 text-stone-300">
+      {/* SAVE BUTTON */}
+      <div className="absolute top-8 right-10 ">
+        <button
+          className="flex items-center justify-center h-10 w-20 border-2 rounded-md active:scale-[0.98] transition-all duration-150 ease-in"
+          onClick={handleSave}
+        >
+          <span>Save</span>
+        </button>
+      </div>
       <Editor
         editorState={editorState}
         onChange={handleChange}
-        placeholder="Start writing..."
+        placeholder="Start Typing...."
       />
     </div>
   );
